5a7b7b200315c35288830c7b7162145f9f26b55c
LibXZR <i@xzr.moe>
2022-02-26 13:28:18 +0800

    walt: Set default window size to 8ms
    On stock ROM, the window size get automatically updated on refresh
    rate changing, which uses 8ms for 120Hz, 12ms for 90Hz and 20ms for
    60Hz. But on custom ROM, there's no userspace daemon to do this,
    causing the windows size to stuck at 20ms.
    
    Signed-off-by: LibXZR <i@xzr.moe>

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -18,7 +18,7 @@
  * Tick interval becomes to 3333333 due to
  * rounding error when HZ=300.
  */
-#define DEFAULT_SCHED_RAVG_WINDOW (3333333 * 6)
+#define DEFAULT_SCHED_RAVG_WINDOW 8000000
 #else
 /* Default window size (in ns) = 20ms */
 #define DEFAULT_SCHED_RAVG_WINDOW 20000000
